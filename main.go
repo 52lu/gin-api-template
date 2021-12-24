d20e870c2bce0ef287e1407b4b8da14c20fb9bdb
liuqinghui <liuqinghui@weimiao.cn>
2021-12-24 19:19:33 +0800

    u

--- main.go
+++ main.go
@@ -2,36 +2,13 @@ package main
 
 import (
 	"52lu/go-import-template/core"
-	"52lu/go-import-template/global"
 	"52lu/go-import-template/initialize"
 )
 
-func init() {
-	// 捕获启动时错误
-	defer global.CatchError()
-	// 程序退出前释放资源
-	defer closeResource()
-	// 初始化全局配置文件
-	initialize.InitConfig()
-	// 初始化zap日志
-	initialize.InitLogger()
-	// 初始化gorm
-	initialize.InitGorm()
-	// 初始化redis
-	initialize.InitRedis()
-	// 初始化es
-	initialize.InitES()
-}
-
 func main() {
+	// 初始化配置
+	initialize.InitViperConfig()
 	// 启动服务
 	core.RunServer()
 }
-// 程序退出前释放资源
-func closeResource()  {
-	// 关闭数据库连接
-	if global.GvaMysqlClient != nil {
-		db, _ := global.GvaMysqlClient.DB()
-		_ = db.Close()
-	}
-}
+
